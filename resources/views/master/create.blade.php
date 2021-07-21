46c9bc62f1ad9b0c652e4255add6e5b8cf3142e3
Eglė Balčiūnaitė <balciunaitegle@gmail.com>
2021-07-21 19:52:02 +0300

    upd

--- resources/views/master/create.blade.php
+++ resources/views/master/create.blade.php
@@ -11,13 +11,13 @@
                  <form method="POST" action="{{route('master.store')}}">
                      <div class="form-group">
                         <label>Name: </label>
-                        <input type="text" class="form-control" name="master_name">
+                        <input type="text" class="form-control" name="master_name" value="{{old('master_name')}}">
                         <small class="form-text text-muted">Masters name.</small>
                     </div>
 
                     <div class="form-group">
                         <label>Surname: </label>
-                        <input type="text" class="form-control" name="master_surname">
+                        <input type="text" class="form-control" name="master_surname" value="{{old('master_surname')}}">
                         <small class="form-text text-muted">Masters surname.</small>
                     </div>
                      @csrf
--- resources/views/master/edit.blade.php
+++ resources/views/master/edit.blade.php
@@ -11,13 +11,13 @@
                  <form method="POST" action="{{route('master.update',$master)}}">
                     <div class="form-group">
                         <label>Name: </label>
-                        <input type="text" class="form-control" name="master_name" value="{{$master->name}}">
+                        <input type="text" class="form-control" name="master_name" value="{{old('master_name', $master->name)}}">
                         <small class="form-text text-muted">Masters name.</small>
                     </div>
 
                     <div class="form-group">
                         <label>Surname: </label>
-                        <input type="text" class="form-control" name="master_surname" value="{{$master->surname}}">
+                        <input type="text" class="form-control" name="master_surname" value="{{old('master_surname', $master->surname)}}">
                         <small class="form-text text-muted">Masters surname.</small>
                     </div>
                      @csrf
